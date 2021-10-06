87b7d8a34b2adac08d61851c56fbc50ff0f137bc
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-10-06 18:21:17 -0300

    labo 06 ej2

--- Laboratorio/06/template-alumnos/ej2/Test/llenarTaxisTEST.cpp
+++ Laboratorio/06/template-alumnos/ej2/Test/llenarTaxisTEST.cpp
@@ -192,3 +192,298 @@ TEST(TaxisTest, Dominio1_Tres_Implementacion3)
     // check
     EXPECT_EQ(result, 5);
 }
+
+/*
+    2_Impar_Par_Impar: 
+    g1 = 5
+    g2 = 2
+    g3 = 1
+    se puede llenar mínimamente 3 taxis de esta manera:
+
+    [3,1]
+    [2,2]
+    [1,1,1,1]
+
+*/
+
+TEST(TaxisTest, Dominio2_Impar_Par_Impar_Implementacion1)
+{
+    // setup
+    int g1 = 5;
+    int g2 = 2;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis1(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 3);
+}
+
+TEST(TaxisTest, Dominio2_Impar_Par_Impar_Implementacion2)
+{
+    // setup
+    int g1 = 5;
+    int g2 = 2;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis2(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 3);
+}
+
+TEST(TaxisTest, Dominio2_Impar_Par_Impar_Implementacion3)
+{
+    // setup
+    int g1 = 5;
+    int g2 = 2;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis3(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 3);
+}
+
+/*
+    2_Impar_Par_Par: 
+    g1 = 5
+    g2 = 2
+    g3 = 2
+    se puede llenar mínimamente 4 taxis de esta manera:
+
+    [3,1]
+    [3,1]
+    [2,2]
+    [1,1,1]
+
+*/
+
+TEST(TaxisTest, Dominio2_Impar_Par_Par_Implementacion1)
+{
+    // setup
+    int g1 = 5;
+    int g2 = 2;
+    int g3 = 2;
+    //exercise
+    int result = llenarTaxis1(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 4);
+}
+
+TEST(TaxisTest, Dominio2_Impar_Par_Par_Implementacion2)
+{
+    // setup
+    int g1 = 5;
+    int g2 = 2;
+    int g3 = 2;
+    //exercise
+    int result = llenarTaxis2(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 4);
+}
+
+TEST(TaxisTest, Dominio2_Impar_Par_Par_Implementacion3)
+{
+    // setup
+    int g1 = 5;
+    int g2 = 2;
+    int g3 = 2;
+    //exercise
+    int result = llenarTaxis3(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 4);
+}
+
+/*
+    2_Par_Par_Impar: 
+    g1 = 4
+    g2 = 2
+    g3 = 1
+    se puede llenar mínimamente 3 taxis de esta manera:
+
+    [3,1]
+    [2,2]
+    [1,1,1]
+
+*/
+
+TEST(TaxisTest, Dominio2_Par_Par_Impar_Implementacion1)
+{
+    // setup
+    int g1 = 4;
+    int g2 = 2;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis1(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 3);
+}
+
+TEST(TaxisTest, Dominio2_Par_Par_Impar_Implementacion2)
+{
+    // setup
+    int g1 = 4;
+    int g2 = 2;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis2(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 3);
+}
+
+TEST(TaxisTest, Dominio2_Par_Par_Impar_Implementacion3)
+{
+    // setup
+    int g1 = 4;
+    int g2 = 2;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis3(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 3);
+}
+
+/*
+    2_Par_Par_Par: 
+    g1 = 4
+    g2 = 2
+    g3 = 2
+    se puede llenar mínimamente 4 taxis de esta manera:
+
+    [3,1]
+    [3,1]
+    [2,2]
+    [1,1]
+
+*/
+
+TEST(TaxisTest, Dominio2_Par_Par_Par_Implementacion1)
+{
+    // setup
+    int g1 = 4;
+    int g2 = 2;
+    int g3 = 2;
+    //exercise
+    int result = llenarTaxis1(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 4);
+}
+
+TEST(TaxisTest, Dominio2_Par_Par_Par_Implementacion2)
+{
+    // setup
+    int g1 = 4;
+    int g2 = 2;
+    int g3 = 2;
+    //exercise
+    int result = llenarTaxis2(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 4);
+}
+
+TEST(TaxisTest, Dominio2_Par_Par_Par_Implementacion3)
+{
+    // setup
+    int g1 = 4;
+    int g2 = 2;
+    int g3 = 2;
+    //exercise
+    int result = llenarTaxis3(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 4);
+}
+
+/*
+    2_Impar_Impar_Impar: 
+    g1 = 3
+    g2 = 1
+    g3 = 1
+    se puede llenar mínimamente 2 taxis de esta manera:
+
+    [3,1]
+    [2,1,1]
+
+*/
+
+TEST(TaxisTest, Dominio2_Impar_Impar_Impar_Implementacion1)
+{
+    // setup
+    int g1 = 3;
+    int g2 = 1;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis1(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 2);
+}
+
+TEST(TaxisTest, Dominio2_Impar_Impar_Impar_Implementacion2)
+{
+    // setup
+    int g1 = 3;
+    int g2 = 1;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis2(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 2);
+}
+
+TEST(TaxisTest, Dominio2_Impar_Impar_Impar_Implementacion3)
+{
+    // setup
+    int g1 = 3;
+    int g2 = 1;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis3(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 2);
+}
+
+/*
+    2_Par_Impar_Impar: 
+    g1 = 4
+    g2 = 1
+    g3 = 1
+    se puede llenar mínimamente 3 taxis de esta manera:
+
+    [3,1]
+    [2,1,1]
+    [1]
+
+*/
+
+TEST(TaxisTest, Dominio2_Par_Impar_Impar_Implementacion1)
+{
+    // setup
+    int g1 = 4;
+    int g2 = 1;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis1(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 3);
+}
+
+TEST(TaxisTest, Dominio2_Par_Impar_Impar_Implementacion2)
+{
+    // setup
+    int g1 = 4;
+    int g2 = 1;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis2(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 3);
+}
+
+TEST(TaxisTest, Dominio2_Par_Impar_Impar_Implementacion3)
+{
+    // setup
+    int g1 = 4;
+    int g2 = 1;
+    int g3 = 1;
+    //exercise
+    int result = llenarTaxis3(g1, g2, g3);
+    // check
+    EXPECT_EQ(result, 3);
+}
