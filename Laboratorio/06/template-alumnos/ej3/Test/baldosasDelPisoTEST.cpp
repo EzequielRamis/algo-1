7b1f6b305ee51875d6d337c78c3ec61480422011
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-10-06 18:45:52 -0300

    labo 06 ej3.1

--- Laboratorio/06/template-alumnos/ej3/Test/baldosasDelPisoTEST.cpp
+++ Laboratorio/06/template-alumnos/ej3/Test/baldosasDelPisoTEST.cpp
@@ -2,3 +2,69 @@
 #include "gtest/gtest.h"
 
 // Escribir tests aca:
+
+// ..|..|..
+// ..|..|..
+// --------
+// ..|..|..
+// ..|..|..
+TEST(BaldosasTest, BDivideAMYN)
+{
+    // setup
+    int M = 4;
+    int N = 6;
+    int B = 2;
+    //exercise
+    int result = baldosasDelPiso(M, N, B);
+    // check
+    EXPECT_EQ(result, 6);
+}
+
+// ..|..|.
+// ..|..|.
+// --------
+// ..|..|.
+// ..|..|.
+TEST(BaldosasTest, BDivideAM)
+{
+    // setup
+    int M = 4;
+    int N = 5;
+    int B = 2;
+    //exercise
+    int result = baldosasDelPiso(M, N, B);
+    // check
+    EXPECT_EQ(result, 5);
+}
+
+// ..|..|..
+// ..|..|..
+// --------
+// ..|..|..
+TEST(BaldosasTest, BDivideAN)
+{
+    // setup
+    int M = 3;
+    int N = 6;
+    int B = 2;
+    //exercise
+    int result = baldosasDelPiso(M, N, B);
+    // check
+    EXPECT_EQ(result, 5);
+}
+
+// ..|..|.
+// ..|..|.
+// --------
+// ..|..|.
+TEST(BaldosasTest, BNoDivide)
+{
+    // setup
+    int M = 3;
+    int N = 5;
+    int B = 2;
+    //exercise
+    int result = baldosasDelPiso(M, N, B);
+    // check
+    EXPECT_EQ(result, 4);
+}
--- Laboratorio/06/template-alumnos/ej3/baldosasDelPiso.cpp
+++ Laboratorio/06/template-alumnos/ej3/baldosasDelPiso.cpp
@@ -1,4 +1,9 @@
-int baldosasDelPiso(int M, int N, int B) {
-	return 0;
-	// Borrar el return dummy y completar
+int baldosasDelPiso(int M, int N, int B)
+{
+	int areaPiso = M * N;
+	int areaBaldosa = B * B;
+	int cantBaldosas = areaPiso / areaBaldosa;
+	if (cantBaldosas * areaBaldosa < areaPiso)
+		cantBaldosas++;
+	return cantBaldosas;
 }
