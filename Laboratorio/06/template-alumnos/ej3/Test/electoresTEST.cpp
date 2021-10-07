6c570af604297cd5fe7a12f1b79bfae65d177225
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-10-07 00:03:26 -0300

    labo 06 ej3.2

--- Laboratorio/06/template-alumnos/ej3/Test/electoresTEST.cpp
+++ Laboratorio/06/template-alumnos/ej3/Test/electoresTEST.cpp
@@ -2,3 +2,122 @@
 #include "gtest/gtest.h"
 
 // Escribir tests aca:
+TEST(EleccionesTest, FechaInvalida1)
+{
+    // setup
+    int dia = 15;
+    int mes = 11;
+    int ano = 2021;
+    //exercise
+    int result = validarVotante(ano, mes, dia);
+    // check
+    EXPECT_EQ(result, ERROR);
+}
+
+TEST(EleccionesTest, FechaInvalida2)
+{
+    // setup
+    int dia = 15;
+    int mes = 11;
+    int ano = 1810;
+    //exercise
+    int result = validarVotante(ano, mes, dia);
+    // check
+    EXPECT_EQ(result, ERROR);
+}
+
+TEST(EleccionesTest, FechaInvalida3)
+{
+    // setup
+    int dia = 15;
+    int mes = 15;
+    int ano = 2021;
+    //exercise
+    int result = validarVotante(ano, mes, dia);
+    // check
+    EXPECT_EQ(result, ERROR);
+}
+
+TEST(EleccionesTest, FechaInvalida4)
+{
+    // setup
+    int dia = 34;
+    int mes = 7;
+    int ano = 2021;
+    //exercise
+    int result = validarVotante(ano, mes, dia);
+    // check
+    EXPECT_EQ(result, ERROR);
+}
+
+TEST(EleccionesTest, FechaInvalida5)
+{
+    // setup
+    int dia = 31;
+    int mes = 4;
+    int ano = 2021;
+    //exercise
+    int result = validarVotante(ano, mes, dia);
+    // check
+    EXPECT_EQ(result, ERROR);
+}
+
+TEST(EleccionesTest, FechaInvalida6)
+{
+    // setup
+    int dia = 29;
+    int mes = 2;
+    int ano = 2019;
+    //exercise
+    int result = validarVotante(ano, mes, dia);
+    // check
+    EXPECT_EQ(result, ERROR);
+}
+
+TEST(EleccionesTest, NoVotante)
+{
+    // setup
+    int dia = 13;
+    int mes = 2;
+    int ano = 2007;
+    //exercise
+    int result = validarVotante(ano, mes, dia);
+    // check
+    EXPECT_EQ(result, NO_VOTA);
+}
+
+TEST(EleccionesTest, VotanteOpcionalMenor)
+{
+    // setup
+    int dia = 13;
+    int mes = 12;
+    int ano = 2004;
+    //exercise
+    int result = validarVotante(ano, mes, dia);
+    // check
+    EXPECT_EQ(result, OPCIONAL_MENOR);
+}
+
+TEST(EleccionesTest, VotanteOpcionalMayor)
+{
+    // setup
+    int dia = 5;
+    int mes = 3;
+    int ano = 1930;
+    //exercise
+    int result = validarVotante(ano, mes, dia);
+    // check
+    EXPECT_EQ(result, OPCIONAL_MAYOR);
+}
+
+TEST(EleccionesTest, Votante)
+{
+    // setup
+    int dia = 14;
+    int mes = 11;
+    int ano = 1980;
+    //exercise
+    int result = validarVotante(ano, mes, dia);
+    // check
+    EXPECT_EQ(result, OBLIGATORIO);
+}
--- Laboratorio/06/template-alumnos/ej3/electores.cpp
+++ Laboratorio/06/template-alumnos/ej3/electores.cpp
@@ -1,6 +1,36 @@
 #include "electores.h"
 
-int validarVotante(int a, int m, int d) {
-	return NO_VOTA;
-	// Borrar el return dummy y completar
+int edadEnFecha(int nd, int nm, int na, int fd, int fm, int fa)
+{
+	int edad = fa - na;
+	if ((fd < nd && fm == nm) || fm < nm)
+		edad--;
+	return edad;
+}
+
+int validarVotante(int a, int m, int d)
+{
+	/* Fecha de elecciones */
+	int dia = 14, mes = 11, ano = 2021;
+	int edadMaxima = 150;
+	/* Valido rangos */
+	if (a >= ano && m >= mes && d >= dia)
+		return ERROR;
+	if (a < ano - edadMaxima || a > ano || m <= 0 || m > 12 || d <= 0 || d > 31)
+		return ERROR;
+	/* Valido meses de 30 días */
+	if (d == 31 && (m == 4 || m == 6 || m == 9 || m == 11))
+		return ERROR;
+	/* Valido años bisiestos */
+	bool esAnoBisiesto = a % 4 == 0 && a % 400 == 0;
+	if (!esAnoBisiesto && m == 2 && d > 28)
+		return ERROR;
+	/* Fin de datos validados */
+	if (edadEnFecha(d, m, a, dia, mes, ano) < 16)
+		return NO_VOTA;
+	if (edadEnFecha(d, m, a, dia, mes, ano) >= 16 && edadEnFecha(d, m, a, dia, mes, ano) < 18)
+		return OPCIONAL_MENOR;
+	if (edadEnFecha(d, m, a, dia, mes, ano) >= 70)
+		return OPCIONAL_MAYOR;
+	return OBLIGATORIO;
 }
